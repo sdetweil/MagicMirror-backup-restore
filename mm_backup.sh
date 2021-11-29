78e639202c60ad9e400c4bfea1509aba0cde9663
Sam Detweiler <sdetweil@gmail.com>
2021-11-29 09:08:46 -0600

    1st release

---
+++ mm_backup.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+#  backup MM modules and config 
+
+base=$HOME/MagicMirror
+saveDir=$HOME/MM_backup
+# is this a mac
+mac=$(uname -s)
+
+if [ $mac == 'Darwin' ]; then
+	cmd=greadlink
+else
+	cmd=readlink
+fi
+msg_prefix='updating'
+script_dir=$(dirname $($cmd -f "$0"))
+if [ ! -d $saveDir ]; then
+	mkdir $saveDir
+	cd $saveDir
+	git init -b main>/dev/null
+	cd - >/dev/null
+	msg_prefix='creating'
+else
+	if [ ! -d $saveDir/.git ]; then
+		cd $saveDir
+		git init -b main>/dev/null
+		cd - >/dev/null
+	fi
+fi
+repo_list=$saveDir/module_list
+
+while getopts "hs:b:" opt
+do
+    case $opt in
+    	# help
+    h) echo $0 takes 2 optional parameters
+			echo -e "\t -s source_dir"
+			echo and
+			echo -e "\t -b backup_dir "
+			exit 1
+	 ;;
+    s)
+		# source MagicMirror folder
+			if [ -d $HOME/$OPTARG ]; then
+				base=$HOME/$OPTARG
+			else
+				if [ -d $OPTARG ]; then
+					base=$OPTARG
+				else
+					echo unable to find Source folder $OPTARG
+					exit 2
+				fi
+			fi
+			echo source MagicMirror folder is $base ;
+    ;;
+    b)
+		# backup folder
+			if [ -d $HOME/$OPTARG ]; then
+				saveDir=$HOME/$OPTARG
+			else
+				if [ -d $OPTARG ]; then
+					saveDir=$OPTARG
+				else
+					echo unable to find backup folder $OPTARG
+					exit 2
+				fi
+			fi
+			echo backup folder is $saveDir;
+    ;;
+    *) printf "Illegal option '-%s'\n" "$opt" && exit 3
+	 ;;
+    esac
+done
+
+echo $msg_prefix folder $saveDir
+#copy config.js
+cp -p $base/config/config.js $saveDir
+
+	SAVEIFS=$IFS   # Save current IFS
+	IFS=$'\n'
+	# get the installed module list
+	# split putput on new lines, not spaces
+	modules=($(find $base/modules -maxdepth 1 -type d | grep -v default | xargs -i echo "{}"))
+	IFS=$SAVEIFS
+# if there is a modules list, erase it, creating new
+if [ ${#modules[@]} -gt 0 ]; then
+	rm $repo_list >/dev/null
+fi
+# loop thru the modules discovered
+for module in "${modules[@]}"
+do
+	# if its not the base modules folder
+	if [ "$module" != "$base/modules" ]; then
+
+		# change to the that module folder
+		cd "$module"
+			# if it has a git repo, then it was cloned
+			if [ -d ".git" ]; then
+				# get the remote repo url
+			    repo=$(git remote -v | grep -m1 git | awk '{print $2}')
+			    # just the module name, not the path
+			    echo found module $(echo $module |awk -F/ '{print $NF}')
+			    echo -e " \t installed from $repo"
+			    # save it to the file
+			    echo $repo >>$repo_list
+			fi
+		# back to the current folder
+		cd - >/dev/null
+	fi
+done
+cd $saveDir
+# add all the changed files
+git add .
+# commit them to the local repo
+git commit -m "updated on $(date)"
+# tag this update with date/time
+git tag -a $(date "+%d-%b-%Y-%H-%M-%S") -m "backup on $(date)"
+echo backup completed, see the git repo at $saveDir
+echo recommended you "git push --tags" from this folder to your backup repo on github
+echo see this link for how to fetch tags for restore
+echo https://devconnected.com/how-to-list-git-tags/
+cd - >/dev/null
---
+++ mm_restore.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+#  backup MM modules and config 
+
+base=$HOME/MagicMirror
+saveDir=$HOME/MM_backup
+# is this a mac
+mac=$(uname -s)
+
+
+while getopts "hs:b:" opt
+do
+    case $opt in
+    	# help
+    h) echo $0 takes 2 optional parameters
+			echo -e "\t -s MagicMirror_dir"
+			echo and
+			echo -e "\t -b backup_dir "
+			exit 1
+	 ;;
+    s)
+		# source MagicMirror folder
+			if [ -d $HOME/$OPTARG ]; then
+				base=$HOME/$OPTARG
+			else
+				if [ -d $OPTARG ]; then
+					base=$OPTARG
+				else
+					echo unable to find MagicMirror folder $OPTARG
+					exit 2
+				fi
+			fi
+			echo MagicMirror folder to update is $base ;
+    ;;
+    b)
+		# backup folder
+			if [ -d $HOME/$OPTARG ]; then
+				saveDir=$HOME/$OPTARG
+			else
+				if [ -d $OPTARG ]; then
+					saveDir=$OPTARG
+				else
+					echo unable to find backup folder $OPTARG
+					exit 2
+				fi
+			fi
+			echo backup folder is $saveDir;
+    ;;
+    *) printf "Illegal option '-%s'\n" "$opt" && exit 3
+	 ;;
+    esac
+done
+
+if [ $mac == 'Darwin' ]; then
+	cmd=greadlink
+else
+	cmd=readlink
+fi
+echo restoring MM configuration from $saveDir to $base
+
+if [ ! -d $saveDir ]; then
+	echo Backup directory $saveDir not found, exiting
+	exit 1
+fi
+repo_list=$saveDir/module_list
+
+if [ -e $repo_list ]; then
+
+	SAVEIFS=$IFS   # Save current IFS
+	IFS=$'\n'
+	# split output on new lines, not spaces
+	urls=($(cat $repo_list))
+	IFS=$SAVEIFS
+
+	# if there modules to restore
+	if [ ${#urls} -gt 0 ]; then
+
+		# restore the config for MM
+		cp -p $saveDir/config.js $base/config
+
+		# restore the repos
+
+		# loop thru the modules listed
+		cd $base/modules
+		for repo_url in "${urls[@]}"
+		do
+			module=$(echo $repo_url | awk -F/ '{print $(NF)}' | awk -F. '{print $1}')
+			if [ ! -d $module ]; then
+				echo restoring $module
+				gc=$(git clone $repo_url >/dev/null)
+				gc_rc=$?
+				if [ $gc_rc -eq 0 ]; then
+					cd $module
+					if [ -e package.json ]; then
+						npm install >/dev/null
+					fi
+					cd - >/dev/null
+				fi
+			else
+				echo module folder $module already exists, skipping
+			fi
+		done
+	fi
+	echo restore completed, u can start MagicMirror now
+else
+	echo no saved module repo list
+fi
