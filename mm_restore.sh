04a9d9d7a211f1c92d243e87190fcebcfc8a488a
Sam Detweiler <sdetweil@gmail.com>
2022-01-02 10:07:30 -0600

    add handling of missing requires, copied from update

--- mm_restore.sh
+++ mm_restore.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+known_list="request valid-url"
+
 #  backup MM modules and config 
 
 base=$HOME/MagicMirror
 saveDir=$HOME/MM_backup
 # is this a mac
 mac=$(uname -s)
-
 
 while getopts "hs:b:" opt
 do
@@ -83,26 +84,66 @@ if [ -e $repo_list ]; then
 
 		# restore the repos
 
-		# loop thru the modules listed
+
 		cd $base/modules
-		for repo_url in "${urls[@]}"
-		do
-			module=$(echo $repo_url | awk -F/ '{print $(NF)}' | awk -F. '{print $1}')
-			if [ ! -d $module ]; then
-				echo restoring $module
-				gc=$(git clone $repo_url >/dev/null)
-				gc_rc=$?
-				if [ $gc_rc -eq 0 ]; then
-					cd $module
-					if [ -e package.json ]; then
-						npm install >/dev/null
+
+			# loop thru the modules listed
+			for repo_url in "${urls[@]}"
+			do
+				module=$(echo $repo_url | awk -F/ '{print $(NF)}' | awk -F. '{print $1}')
+				if [ ! -d $module ]; then
+					echo restoring $module
+					gc=$(git clone $repo_url >/dev/null)
+					gc_rc=$?
+					if [ $gc_rc -eq 0 ]; then
+						cd $module
+						if [ -e package.json ]; then
+							npm install &>/dev/null
+						fi
+						cd - >/dev/null
 					fi
-					cd - >/dev/null
 				fi
-			else
-				echo module folder $module already exists, skipping
-			fi
-		done
+			done
+
+			# lets check for modules with missing requires (libs removed from MM base)
+			# this skips any default modules
+			echo Checking for modules with removed libraries
+			mods=($(find . -maxdepth 2 -type f -name  node_helper.js | awk -F/ '{print $2}'))
+
+			# loop thru all the installed modules
+			for  mod in "${mods[@]}"
+			do
+				# get the require statements from the node helper
+				requires=($(grep -e "require(" $mod/node_helper.js | awk -F '[()]' '{print $2}' | tr -d '"' | tr -d "'"))
+				# loop thru the requires
+				for require in "${requires[@]}"
+				do
+					# check it against the list of known lib removals
+					case " $known_list " in (*" $require "*) :;; (*) false;; esac
+					# if found in the list
+					if [ $? == 0 ]; then
+						# if no package.json, we would have to create one
+						cd $mod
+						if [ ! -e package.json ]; then
+							echo -e ' \n\t ' package.json not found for module $mod
+							echo adding package.json for module $mod
+							npm init -y >/dev/null
+						fi
+						# if package.json exists, could have been just added
+						if [ -e package.json ]; then
+							# check for this library in the package.json
+							pk=$(grep $require package.json)
+							# if not present, need to do install
+							if [ "$pk." == "." ]; then
+								echo -e ' \n\t 'require for $require in module $mod not found in package.json package.json for module $mod
+								echo installing $require for module $mod
+								npm install $require --save
+							fi
+						fi
+						cd - >/dev/null
+					fi
+				done
+			done
 	fi
 	echo restore completed, u can start MagicMirror now
 else
